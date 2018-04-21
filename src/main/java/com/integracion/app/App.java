0d74dea9cae3c2f78d5e253ddf12dde567045633
Dani <pangea@pangea-pc>
2018-04-21 16:39:43 +0200

    new

--- src/main/java/com/integracion/app/App.java
+++ src/main/java/com/integracion/app/App.java
@@ -6,8 +6,20 @@ package com.integracion.app;
  */
 public class App 
 {
+	
+	public int suma(int a, int b) {
+    	return a+b;
+    }
+	    
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello Jose!" );
+        App i = new App();
+        System.out.println(i.suma(1, 2));
+        
     }
+    
+ 
+    
 }
