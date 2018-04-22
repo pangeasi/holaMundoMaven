a67b8cf597aa748d9846e28d52d24f385f5e8d6d
Dani <pangea@pangea-pc>
2018-04-22 11:35:20 +0200

    new

--- src/main/java/com/integracion/app/App.java
+++ src/main/java/com/integracion/app/App.java
@@ -18,7 +18,7 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jose!" );
+        System.out.println( "Hello Jandemor!" );
         
         System.out.println(suma(1, 2));
         System.out.println(resta(2, 7));
